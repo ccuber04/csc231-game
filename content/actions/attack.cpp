95e8c92a03fa8b92e0d1b36f445dfceceb72c4b7
Connal Meinertz <connal.meinertz@outlook.com>
2023-05-03 10:26:55 -0500

    attack class

---
+++ content/actions/attack.cpp
@@ -0,0 +1,16 @@
+#include "attack.h"
+
+#include <iostream>
+
+#include "actor.h"
+#include "rest.h"
+
+Attack::Attack(Actor& defender) : defender{defender} {}
+
+Result Attack::perform(Engine&) {
+    if (actor->team != defender.team) {
+        actor->attack(defender);
+        return success();
+    }
+    return alternative(Rest{});
+}
