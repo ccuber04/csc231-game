f9cb12376b7243a0a1e321b354deb6d30b72c730
Connal Meinertz <connal.meinertz@outlook.com>
2023-05-09 09:59:49 -0500

    added thrust to knife

--- content/weapons/knife.cpp
+++ content/weapons/knife.cpp
@@ -1,10 +1,11 @@
 #include "knife.h"
 
 #include "engine.h"
-#include "hit.h"
+#include "thrust.h"
 
 Knife::Knife(int damage) : Weapon{"knife", damage} {}
 
-void Knife::use(Engine& engine, Actor&, Actor& defender) {
-    engine.events.add(Hit{defender, damage});
+void Knife::use(Engine& engine, Actor& attacker, Actor& defender) {
+    Vec direction = defender.get_position() - attacker.get_position();
+    engine.events.add(Thrust{sprite, direction, defender, damage});
 }
